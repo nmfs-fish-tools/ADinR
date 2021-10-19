6c856f4b4da5dec937c204041a003dc0be4fb612
msupernaw <matthew.supernaw@noaa.gov>
2021-10-19 16:44:40 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onLoad <- function(libname, pkgname) {
   print("loading ADinR")
   library.dynam("ADinR", pkgname, libname)
-
+  source("operators.R")
 }
 
-source("operators.R")
+
 
 .LastLib <- function(libpath)
  {
