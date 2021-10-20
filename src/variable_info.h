32d221df67bcc04d2607d7ac92539ebd2924ef45
msupernaw <matthew.supernaw@noaa.gov>
2021-10-20 12:55:30 -0400

    Update variable_info.h

--- src/variable_info.h
+++ src/variable_info.h
@@ -130,7 +130,7 @@ struct variable_info {
         }
     }
 };
-variable_info::transformation = new logit_transformation();
+transformation variable_info::transformation = new logit_transformation();
 size_t variable_info::id_g = 0;
 
 
